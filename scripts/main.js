4387d7710e59af4242b20ef84e48b8d49fa91d69
moonlight52137 <60054309+moonlight52137@users.noreply.github.com>
2020-09-25 17:11:01 +0800

    gai2

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+﻿document.querySelector('h2').onclick = function() {
+    alert('点击事件');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/text.jpg') {
+      myImage.setAttribute('src', 'images/text2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/text.jpg');
+    }
+}
+
+
